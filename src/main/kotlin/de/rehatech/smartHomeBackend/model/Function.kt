f5c06ca5fd87ca56921290aa0d53effa9862d64b
Sebastian <1sebastian1@online.de>
2022-12-28 18:59:06 +0100

    Erstelle neue Entitys

---
+++ src/main/kotlin/de/rehatech/smartHomeBackend/model/Function.kt
@@ -0,0 +1,13 @@
+package de.rehatech.smartHomeBackend.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Function(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+)
