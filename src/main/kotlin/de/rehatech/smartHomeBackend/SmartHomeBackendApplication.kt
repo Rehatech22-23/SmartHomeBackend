fb79dc35d90632b4d37dd3f7b16218574ff5994a
Sebastian <1sebastian1@online.de>
2022-12-28 19:00:38 +0100

    Füge neue Beans ein

--- src/main/kotlin/de/rehatech/smartHomeBackend/SmartHomeBackendApplication.kt
+++ src/main/kotlin/de/rehatech/smartHomeBackend/SmartHomeBackendApplication.kt
@@ -1,9 +1,15 @@
 package de.rehatech.smartHomeBackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+
 
 @SpringBootApplication
+@EntityScan("de.rehatech.smartHomeBackend.model")
+@EnableJpaRepositories("de.rehatech.smartHomeBackend.repositories")
 class SmartHomeBackendApplication
 
 fun main(args: Array<String>) {
