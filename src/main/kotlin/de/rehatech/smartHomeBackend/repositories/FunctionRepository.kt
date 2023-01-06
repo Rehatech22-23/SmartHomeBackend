302a1883679d0148487024159ceef928899ef267
Sebastian <1sebastian1@online.de>
2023-01-06 18:54:04 +0100

    Rename

--- src/main/kotlin/de/rehatech/smartHomeBackend/repositories/FunctionRepository.kt
+++ src/main/kotlin/de/rehatech/smartHomeBackend/repositories/FunctionRepository.kt
@@ -2,8 +2,8 @@ package de.rehatech.smartHomeBackend.repositories
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import de.rehatech.smartHomeBackend.entities.Function
+import de.rehatech.smartHomeBackend.entities.FunctionValues
 
 @Repository
-interface FunctionRepository : CrudRepository<Function, Long> {
+interface FunctionRepository : CrudRepository<FunctionValues, Long> {
 }
