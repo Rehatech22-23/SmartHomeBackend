e0616f8d756ade4c0694315ccdccd936ffe0e4c6
Sofia <sofia.bonas@tu-dortmund.de>
2023-01-18 12:12:37 +0100

    added @Autowirded and changed Port to 9000

--- src/main/kotlin/de/rehatech/smartHomeBackend/controller/frontend/DeviceController.kt
+++ src/main/kotlin/de/rehatech/smartHomeBackend/controller/frontend/DeviceController.kt
@@ -2,13 +2,14 @@ package de.rehatech.smartHomeBackend.controller.frontend
 
 import de.rehatech.smartHomeBackend.controller.backend.responsesClass.hilfsclass.Device
 import de.rehatech.smartHomeBackend.services.DeviceService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class DeviceController (private val deviceService: DeviceService) {
+class DeviceController @Autowired constructor(val deviceService: DeviceService) {
 
     @GetMapping("/device/list")
     fun getDeviceIdList(): List<String> = deviceService.getDeviceIdList()
