d6b0e1f913f6b6ae600cb1e9fcb9cbea7a32f2b4
Sebastian <1sebastian1@online.de>
2023-03-29 13:35:25 +0200

    Homee Geräte richtige namen

--- src/main/kotlin/de/rehatech/smartHomeBackend/SmartHomeBackendApplication.kt
+++ src/main/kotlin/de/rehatech/smartHomeBackend/SmartHomeBackendApplication.kt
@@ -1,9 +1,8 @@
 package de.rehatech.smartHomeBackend
 
-import de.rehatech.smartHomeBackend.config.ApiConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -11,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EntityScan("de.rehatech.smartHomeBackend.entities")
-@EnableConfigurationProperties(ApiConfiguration::class)
+@ConfigurationPropertiesScan("de.rehatech.smartHomeBackend.config")
 @EnableJpaRepositories("de.rehatech.smartHomeBackend.repositories")
 @EnableScheduling
 class SmartHomeBackendApplication
