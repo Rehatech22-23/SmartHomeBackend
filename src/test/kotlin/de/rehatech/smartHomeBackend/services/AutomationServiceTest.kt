d673423239e6e17c49f1aa6301ae9a203ea3e880
Sebastian <1sebastian1@online.de>
2023-04-04 23:04:46 +0200

    AutomationServiceTest.kt

--- src/test/kotlin/de/rehatech/smartHomeBackend/services/AutomationServiceTest.kt
+++ src/test/kotlin/de/rehatech/smartHomeBackend/services/AutomationServiceTest.kt
@@ -1,9 +1,34 @@
 package de.rehatech.smartHomeBackend.services
 
+import de.rehatech.smartHomeBackend.databuild.RoutineBuild
+import de.rehatech.smartHomeBackend.repositories.RoutineRepository
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class AutomationServiceTest {
+
+    @Autowired
+    lateinit var  automationService: AutomationService
+
+    @Autowired
+    lateinit var routineService: RoutineService
+
+    @Autowired
+    lateinit var routineRepository: RoutineRepository
+
+    var routineBuild = RoutineBuild()
+
+
+    @Test
+    fun testErrorRoutine()
+    {
+        val mockRoutine = routineService.createRoutine(routineBuild.noRangeRoutine())
+        automationService.automation()
+        assertEquals(1, routineRepository.count())
+    }
 
 
 }
