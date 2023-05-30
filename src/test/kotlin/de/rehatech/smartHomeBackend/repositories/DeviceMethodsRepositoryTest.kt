5e985c1d61529c00abd8f33e4465f250cbe64f2c
Sebastian <1sebastian1@online.de>
2023-05-30 15:46:35 +0200

    Behebe fehler beim Test

--- src/test/kotlin/de/rehatech/smartHomeBackend/repositories/DeviceMethodsRepositoryTest.kt
+++ src/test/kotlin/de/rehatech/smartHomeBackend/repositories/DeviceMethodsRepositoryTest.kt
@@ -3,6 +3,7 @@ package de.rehatech.smartHomeBackend.repositories
 import de.rehatech.smartHomeBackend.enums.FunctionType
 import de.rehatech.smartHomeBackend.entities.DeviceMethods
 import de.rehatech.smartHomeBackend.entities.OpenHabDevice
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -20,13 +21,18 @@ class DeviceMethodsRepositoryTest {
     fun testJoin()
     {
         val test = OpenHabDevice(name="hallo", uid="by")
-        openHabDeviceRepository.save(test)
-        val t = openHabDeviceRepository.findById(1).get()
+        val device = openHabDeviceRepository.save(test)
+        val t = openHabDeviceRepository.findById(device.id!!).get()
+        assertEquals("hallo", t.name)
+        assertEquals("by", t.uid)
         val newFun = DeviceMethods(name = "test", label= "test", type = FunctionType.Switch, deviceOpenHab = t)
         deviceMethodsRepository.save(newFun)
 
 
-        val t3 = openHabDeviceRepository.findById(1).get()
-        println( t3.deviceMethodsIDS.size)
+        val t3 = openHabDeviceRepository.findById(device.id!!).get()
+        assertEquals(1, t3.deviceMethodsIDS.size)
+        openHabDeviceRepository.delete(t3)
+
+
     }
 }
